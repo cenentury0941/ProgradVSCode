345051bb271e4d84fb072f3d4c9e77c808b08882
unknown <cenentury0941@gmail.com>
2023-03-23 12:39:04 +0530

    Day 3 - Lunch Break

--- Day3/ConsoleApp3/ConsoleApp3/Program.cs
+++ Day3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System.Runtime.CompilerServices;
 using System;
+using Microsoft.VisualBasic.FileIO;
 
 namespace ConsoleApp3
 {
     class Program
     {
 
+        int Value;
+
+        public Program( int Val ) {
+            this.Value = Val;
+            Console.WriteLine( "Program Created");
+        }
+
+        ~Program() {
+            Console.WriteLine("Get Wrecked!");
+        }
+
+        public void DisplayValues()
+        {
+            Console.WriteLine( $"Value of Program is : {this.Value}" );
+        }
+
         public enum days { 
         Monday, Tuesday, Wednesday, Thursday, Friday
         }
 
+        static void destroyer()
+        {
+            Program prog = new Program(420);
+            prog.DisplayValues();
+            prog = null;
+            System.GC.Collect();
+        }
+
         static void Main(string[] args)
         {
+            destroyer();
 
             foreach ( days val in Enum.GetValues(typeof(days)) )
             {
@@ -32,11 +58,23 @@ namespace ConsoleApp3
 
             try
             {
-                Console.WriteLine(arr[2,4]);
+                throw new AgeException("FBI");
             }
-            catch( IndexOutOfRangeException ) {
+            catch (IndexOutOfRangeException)
+            {
                 Console.WriteLine("Out of bounds, yo!");
-            }            
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Don't you go dividing by zero now!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected : " + e.Message);
+            }
+            finally {
+                Console.WriteLine("Safe!");
+            }
         }
 
         public void greet()
@@ -44,5 +82,12 @@ namespace ConsoleApp3
             Console.WriteLine("Greeting");
         }
 
+    }
+
+
+
+    public class AgeException : Exception { 
+        public AgeException(string myMessage) : base(myMessage) { 
+        }
     }
 }
