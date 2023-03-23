41ac31b3a8d56f4b32aca6e2fad791b72cb96232
unknown <cenentury0941@gmail.com>
2023-03-23 16:43:28 +0530

    Day 3 - Assignment And Game

---
+++ Day3/Assignment/Assignment/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Net.Http.Headers;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Write a program in C# Sharp to count a 
+            //total number of duplicate elements in 
+            //an array.
+
+            int[] arr = { 1, 2, 3, 3, 2, 4, 5, 1, 2, 8 };
+            HashSet<int> hash = new HashSet<int>(arr);
+            Console.WriteLine( $"Number of duplicate elements : {arr.Length-hash.Count}" );
+
+
+            Console.WriteLine("\n");
+
+            //Write a program in C# Sharp to merge two arrays of 
+            //same size sorted in ascending order.
+
+            int[] arr1 = { 1, 4, 7, 8 };
+            int[] arr2 = { 3, 5, 7, 9 };
+            int[] arr3 = new int[arr1.Length + arr2.Length];
+            int arr1_index = 0, arr2_index = 0, arr3_index = 0;
+            while ( arr1_index < arr1.Length && arr2_index < arr2.Length )
+            {
+                if (arr1[arr1_index] <= arr2[arr2_index] )
+                {
+                    arr3[arr3_index++] = arr1[arr1_index++];
+                }
+                else
+                {
+                    arr3[arr3_index++] = arr2[arr2_index++];
+                }
+            }
+
+            while (arr1_index < arr1.Length)
+            {
+                arr3[arr3_index++] = arr1[arr1_index++];
+            }
+            
+            while (arr2_index < arr2.Length)
+            {
+                arr3[arr3_index++] = arr2[arr2_index++];
+            }
+
+            Console.Write( "Elements of arr3 : " );
+            foreach ( int i in arr3 )
+            {
+                Console.Write( $" {i} |" );
+            }
+
+            Console.WriteLine("\n");
+
+
+            //Write a program in C# Sharp to separate 
+            //odd and even integers in separate arrays
+
+            List<int> Even = new List<int>() , Odd = new List<int>() ;
+
+            foreach ( int i in arr3 )
+            {
+                if ( i%2 == 0 )
+                {
+                    Even.Add(i);
+                }
+                else {
+                    Odd.Add(i);
+                }
+            }
+
+            Console.Write("Even Numbers : ");
+            Even.ForEach(x => Console.Write($"{x} - "));
+            Console.WriteLine();
+
+            Console.Write("Odd Numbers : ");
+            Odd.ForEach(x => Console.Write($"{x} - "));
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Day3/HeroVillianGame/HeroVillianGame/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace HeroVillianGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Thunderdome!\n");
+            string gunman;
+            GameState Game = new GameState();
+            while (Game.H_Remaining_Bullets >= 1 && Game.V_Remaining_Bullets >= 3)
+            {
+                Console.Write("Who fires the bullet? (HERO/VILL) : ");
+                gunman = Console.ReadLine().Trim();
+
+                if (string.Compare(gunman, "HERO") == 0)
+                {
+                    Game.HeroFires();
+                }
+                else if (string.Compare(gunman, "VILL") == 0)
+                {
+                    Game.VillianFires();
+                }
+            }
+            Console.WriteLine( "GAME OVER" );
+            
+        }
+    }
+
+    class GameState
+    {
+        public int H_Remaining_Bullets, V_Remaining_Bullets;
+        int Hero_Tally;
+        int Villian_Tally;
+
+        public GameState()
+        {
+            this.H_Remaining_Bullets = 10;
+            this.V_Remaining_Bullets = 10;
+            this.Hero_Tally = 0;
+            this.Villian_Tally = 0;
+        }
+
+        public int HeroFires()
+        {
+            this.H_Remaining_Bullets -= 1;
+            this.Hero_Tally += 1;
+            Console.WriteLine("");
+            Console.WriteLine("       ------ > "); 
+            Console.WriteLine("");
+            Console.WriteLine($"Hero Fired 1 Bullet, Remaining Bullets : {this.H_Remaining_Bullets}");
+            Console.WriteLine($"Hero Has Fired {this.Hero_Tally} Bullet(s)");
+            Console.WriteLine($"Villian Has Fired {this.Villian_Tally} Bullet(s)");
+            Console.WriteLine("\n\n\n");
+            return this.H_Remaining_Bullets;
+        }
+
+
+        public int VillianFires()
+        {
+            if ( this.V_Remaining_Bullets < 3 )
+            {
+                Console.WriteLine( "Not Enough Bullets..." );
+                return -1;
+            }
+
+            this.V_Remaining_Bullets -= 3;
+            this.Villian_Tally += 3;
+
+            Console.WriteLine("             < ------ ");
+            Console.WriteLine("       < ------ ");
+            Console.WriteLine("                     < ------ ");
+            Console.WriteLine($"Villian Fired 3 Bullets, Remaining Bullets : {this.V_Remaining_Bullets}");
+            Console.WriteLine($"Hero Has Fired {this.Hero_Tally} Bullet(s)");
+            Console.WriteLine($"Villian Has Fired {this.Villian_Tally} Bullet(s)");
+            Console.WriteLine("\n\n\n");
+
+            return this.V_Remaining_Bullets;
+        }
+
+    }
+
+}
