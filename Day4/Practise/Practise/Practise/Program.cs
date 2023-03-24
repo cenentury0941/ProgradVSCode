83c2e8f258a2039cea2d65ecade44bd7c707d283
unknown <cenentury0941@gmail.com>
2023-03-24 15:49:39 +0530

    Day 4 Practise Added

---
+++ Day4/Practise/Practise/Practise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a C# Sharp program to exchange the first and last characters
+            //in a given string and return the new string
+
+
+            Console.WriteLine("Enter A String : ");
+            string EnteredValue = Console.ReadLine().Trim();
+            string Swapped = EnteredValue[EnteredValue.Length-1] + EnteredValue.Substring(1,EnteredValue.Length-2) + EnteredValue[0];
+            Console.WriteLine($"Swapped String : {Swapped}");
+
+            Console.WriteLine( "\n\n\n" );
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
