6ce432fd446f649b14c6edf03e88ccea97f0a6cc
unknown <cenentury0941@gmail.com>
2023-03-24 15:56:22 +0530

    Day 4 Practise Updated

--- Day4/Practise/Practise/Practise/Program.cs
+++ Day4/Practise/Practise/Practise/Program.cs
@@ -13,15 +13,35 @@ namespace Practise
 
             Console.WriteLine("Enter A String : ");
             string EnteredValue = Console.ReadLine().Trim();
-            string Swapped = EnteredValue[EnteredValue.Length-1] + EnteredValue.Substring(1,EnteredValue.Length-2) + EnteredValue[0];
-            Console.WriteLine($"Swapped String : {Swapped}");
-
+            if (EnteredValue.Length <= 1)
+            {
+                Console.WriteLine("String Is Too Short.");
+            }
+            else
+            {
+                string Swapped = EnteredValue[EnteredValue.Length - 1] + EnteredValue.Substring(1, EnteredValue.Length - 2) + EnteredValue[0];
+                Console.WriteLine($"Swapped String : {Swapped}");
+            }
             Console.WriteLine( "\n\n\n" );
 
 
+            //Write a C# Sharp program to create a new string with the last char
+            //added at the front and back of a given string of length 1 or more.
 
 
-
+            Console.WriteLine("Enter A String : ");
+            EnteredValue = Console.ReadLine().Trim();
+            if (EnteredValue.Length < 1)
+            {
+                Console.WriteLine("String Is Too Short.");
+            }
+            else
+            {
+                string Modified = EnteredValue.Last() + EnteredValue + EnteredValue.Last();
+                Console.WriteLine($"Modified String : {Modified}");
+            }
+            
+            Console.WriteLine("\n\n\n");
 
 
 
