43454bfeedaf256f2f1e4b2004af5d4d95a8bd05
unknown <cenentury0941@gmail.com>
2023-03-27 16:50:59 +0530

    Day 5 - Maximum profit added

---
+++ Day5/HandsOn/HandsOn/HandsOn/Ques2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Class1
+{
+
+        public static void Main(string[] args)
+        {
+            int[] Prices = { 7 , 1 , 5 , 3 , 6 , 4 };
+            Console.WriteLine($"Maximum Profit : { getMaximumProfit(Prices , Prices.Length) }");
+        
+        }
+
+        static int getMaximumProfit( int[] Prices , int NumberOfDays )
+        {
+            int MaximumProfit = 0;
+            int MinimumCost = Prices[0] ;
+
+            for ( int Day = 0; Day < NumberOfDays; Day++ )
+            {
+                int ProfitToday = Prices[Day] - MinimumCost;
+                
+                if ( ProfitToday > MaximumProfit )
+                { 
+                    MaximumProfit = ProfitToday;
+                }
+
+                if (Prices[Day] < MinimumCost )
+                { 
+                    MinimumCost = Prices[Day];
+                }
+            }
+
+            return MaximumProfit;
+        
+        }
+
+}
