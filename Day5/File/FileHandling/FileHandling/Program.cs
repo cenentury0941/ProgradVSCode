38b812bfb032c7ffa2a036aa177d28dc017e9a05
unknown <cenentury0941@gmail.com>
2023-03-27 16:07:15 +0530

    Day 5 - Array Hands on added

---
+++ Day5/File/FileHandling/FileHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FileStream fs = new FileStream( "Trojan.txt", FileMode.OpenOrCreate , FileAccess.ReadWrite );
+            fs.Seek( 5, SeekOrigin.Begin );
+            fs.Write(Encoding.ASCII.GetBytes("123456789012345678901234567890"));
+            fs.Close(); 
+            FileStream fs2 = new FileStream("Trojan.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //fs2.Seek(2, SeekOrigin.Begin );
+            //StreamReader sr = new StreamReader(fs2);
+            //byte[] buffer = new byte[128];
+            //fs2.Read(buffer);
+            //string str = Encoding.ASCII.GetString(buffer);
+            fs2.Seek(12, SeekOrigin.Begin);
+            StreamWriter sw = new StreamWriter(fs2);
+            sw.Write(("ppppppppp"));
+            sw.Close();
+
+            fs2.Close();
+            fs2 = new FileStream("Trojan.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamReader sr = new StreamReader(fs2);
+            Console.WriteLine( $"File Content : { sr.ReadToEnd() }" );
+            
+            sr.Close();
+
+            
+
+
+
+        }
+    }
+}
---
+++ Day5/HandsOn/HandsOn/HandsOn/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Spairaru Matoriksu!");
+
+            //int[,] Matrix = new int[4, 4] { { 1 , 2 , 3 , 4 },
+            //                                { 5 , 6 , 7 , 8},
+            //                                { 9, 10 , 11 , 12},
+            //                                { 13 , 14 , 15 , 16} };
+
+            int[,] Matrix = new int[3, 4] { { 1 , 2 , 3 , 4 },
+                                            { 5 , 6 , 7 , 8},
+                                            { 9, 10 , 11 , 12} };
+
+            printSpiralMatrix( Matrix , 3 , 4 );
+
+        }
+
+
+        static void printSpiralMatrix(int[,] Matrix , int rows , int cols)
+        {
+            int Rows = rows, Colls = cols;
+            int X_mod = 1, Y_mod = 0;
+            int X = 0, Y = 0;
+            int XMaxLimit = Colls - 1, XMinLimit = -1;
+            int YMaxLimit = Rows - 1, YMinLimit = 0;
+
+            for (int i = 0; i < Rows * Colls; i++)
+            {
+                Console.Write($"{Matrix[Y, X]} ");
+                if (X == 0 && Y == 0)
+                { }
+                else if (X_mod + X > XMaxLimit)
+                {
+                    X_mod = 0;
+                    Y_mod = 1;
+                    YMinLimit += 1;
+                }
+                else if (X + X_mod < XMinLimit)
+                {
+                    X_mod = 0;
+                    Y_mod = -1;
+                    YMaxLimit -= 1;
+                }
+                else if (Y + Y_mod < YMinLimit)
+                {
+                    Y_mod = 0;
+                    X_mod = 1;
+                    XMaxLimit -= 1;
+                }
+                else if (Y + Y_mod > YMaxLimit)
+                {
+                    Y_mod = 0;
+                    X_mod = -1;
+                    XMinLimit += 1;
+                }
+
+
+                X += X_mod;
+                Y += Y_mod;
+
+            }
+        }
+    }
+}
