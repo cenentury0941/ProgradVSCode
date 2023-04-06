e7baeb624c8858f99ba7c11f47c815d764ca3c0b
unknown <cenentury0941@gmail.com>
2023-04-06 16:39:45 +0530

    Day 12 Problem and SQL Driver

---
+++ Day12/NicoCipher/NicoCipher/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace NicoCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "edabitisamazing";
+            string key = "matt";
+            string encoded = NicoCipher.Encode(message , key);
+            Console.WriteLine( $" Message : '{message}'\n Encoded with Key : '{key}'\n is : '{encoded}'" );
+        }
+    }
+
+    internal class NicoCipher {
+
+        public static string Encode( string message , string key )
+        {
+            key = key.ToLower();
+            int[] colOrder = new int[key.Length];
+            int messageMatrixSize = message.Length / key.Length;
+            messageMatrixSize += (message.Length % key.Length == 0) ? 0 : 1;
+
+            int index = 1;
+            for ( char testChar = (char)0; testChar < 256; testChar++ )
+            {
+                for ( int charIndex = 0; charIndex < key.Length; charIndex++ )
+                {
+                    if ( key[charIndex] == testChar )
+                    {
+                        colOrder[charIndex] = index++;
+                    }
+                }
+            }
+
+            Dictionary<int, string[]> messageMatrix = new Dictionary<int, string[]>();
+
+            for ( int indexKey = 1; indexKey <= key.Length; indexKey++ )
+            {
+                messageMatrix[indexKey] = new string[messageMatrixSize];
+                for( int _ = 0; _ < messageMatrixSize; _++ )
+                {
+                    messageMatrix[indexKey][_] = " "; 
+                }
+            }
+
+
+
+            int charsLeft = message.Length;
+            int matrixRow = 0;
+            int messageIndex = 0;
+            int colIndex = 0;
+            while ( charsLeft > 0 )
+            {
+                messageMatrix[colOrder[colIndex++]][ matrixRow ] = "" + message[messageIndex++];
+                colIndex %= key.Length;
+                if ( colIndex == 0 )
+                {
+                    matrixRow += 1;
+                }
+                charsLeft -= 1;
+            }
+
+
+            /*
+            for (int indexKey = 1; indexKey <= key.Length; indexKey++)
+            {
+                Console.WriteLine(indexKey);
+                for (int _ = 0; _ < messageMatrixSize; _++)
+                {
+                    Console.Write(messageMatrix[indexKey][_]);
+                }
+                Console.WriteLine("\n");
+            }
+            */
+
+            string encodedMessage = "";
+
+            for ( int row = 0; row < messageMatrixSize; row++ ) {
+                for (int col = 1; col <= key.Length; col++)
+                {
+                    encodedMessage += messageMatrix[col][row];
+                }
+            }
+
+            return encodedMessage;
+        }
+
+    }
+}
---
+++ Day12/SQLClient/SQLClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace SQLClient
+{
+    internal class Program
+    {
+        private static string connectionString = "Data Source=DESKTOP-GEG4MT8;Initial Catalog=TrainingDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            DbCommand cmd = conn.CreateCommand();
+            //"SELECT * FROM STUDENTS";
+            //SqlDataReader read = (SqlDataReader)cmd.ExecuteReader();
+            
+            
+            cmd.CommandText = "DELETE FROM LOGS WHERE ID = 0"; 
+            int rowCount = cmd.ExecuteNonQuery();
+
+            if ( rowCount == 0 )
+            {
+                Console.WriteLine( "No Records" );
+            }
+            else {
+
+                Console.WriteLine($" {rowCount} Records Erased");
+            }
+            conn.Close();
+            return;
+
+        }
+    }
+}
