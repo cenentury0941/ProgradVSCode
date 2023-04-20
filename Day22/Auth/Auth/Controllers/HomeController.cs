68069f465178c1c190ea4d22333e61b126c43715
unknown <cenentury0941@gmail.com>
2023-04-20 12:48:46 +0530

    Auth Added

---
+++ Day22/Auth/Auth/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Auth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Auth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine( User.Identity.AuthenticationType );
+            return View();
+        }
+
+        [Authorize(Roles ="Administrator")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day22/Auth/Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentModel> Students { get; set; }
+
+    }
+}
