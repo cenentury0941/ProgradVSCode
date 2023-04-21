0ebdc28c80dc14a41a85c8b4349ec247828266be
unknown <cenentury0941@gmail.com>
2023-04-21 10:21:27 +0530

    Init Mobile Recharge

---
+++ Day23/Recharge/Recharge/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Recharge.Models;
+
+namespace Recharge.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+
+        public DbSet<TestModel> testModels { get; set; }
+
+    }
+}
